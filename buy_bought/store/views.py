5f6a08c462e2e97b96b33f6c9978abf6512957ec
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-11-22 22:20:20 -0300

    :sparkles: feat: added product views

--- buy_bought/store/views.py
+++ buy_bought/store/views.py
@@ -1,19 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Defines functions that execute in response to requests
 
-products = [
-    {'name': 'Pair of boots'},
-    {'name': 'Old IPhone'},
-    {'name': 'Almost new couch'},
-]
+# products = [
+#     {'name': 'Pair of boots'},
+#     {'name': 'Old IPhone'},
+#     {'name': 'Almost new couch'},
+# ]
 
 
 def get_products(request):
-    # Renders template from templates folder
-    return render(request, 'products.html', {'products': products})
+    if request.method == "GET":
+        products = Product.objects.all()
+
+        # Renders template from templates folder
+        return render(request, 'products.html', {'products': products})
+    elif request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+
+        product = Product(
+            name=name,
+            price=price,
+        )
+
+        product.save()
+
+        return HttpResponse({
+            'name': name,
+            'price': price,
+        })
 
 
 def get_product(request):
-    return HttpResponse('Hello, World!')
+    if request.method == "GET":
+        return HttpResponse('GET PRODUCT!')
+    elif request.method == "POST":
+        return HttpResponse('POST PRODUCT!')
+
+
+def new_product(request):
+    return render(request, 'new.html')
