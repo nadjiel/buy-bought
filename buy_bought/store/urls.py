7acfa8886227f9ec65159d0e181e49920c62cb21
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-11-22 22:20:37 -0300

    :sparkles: feat: added product urls

--- buy_bought/store/urls.py
+++ buy_bought/store/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 # Ties routes to views within this app
 urlpatterns = [
-    path('', views.get_products, name='get_products'),
-    path('product', views.get_product, name='get_product'),
+    path('', views.get_products, name='products'),
+    path('products/', views.get_product, name='get_product'),
+    path('products/new', views.new_product, name='new_product'),
 ]
